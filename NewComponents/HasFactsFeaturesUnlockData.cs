6618706ffb318a1342e4da593340b2063cf399e8
Skarm Ghelmaad <skarm.ghelmaad@gmail.com>
2022-09-29 18:07:06 +0200

    - Add HasFactsFeaturesUnlock component - Add HasFactsFeaturesUnlockData

---
+++ NewComponents/HasFactsFeaturesUnlockData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Kingmaker.EntitySystem;
+using Newtonsoft.Json;
+using Kingmaker.Designers.Mechanics.Facts;
+
+namespace LegacyOfShadows.NewComponents
+{
+
+    // This is the equivalent of UnitFactComponentDelegate<AddFeatureIfHasFactData> but works in blocks of facts (array) instead of individual facts.
+    internal class HasFactsFeaturesUnlockData
+    {
+        public EntityFact[] AppliedFacts;
+    }
+}
