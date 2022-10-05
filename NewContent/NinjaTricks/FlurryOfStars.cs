78c9b6125fd537d1dbce795f7632d30f1435d19c
Skarm Ghelmaad <skarm.ghelmaad@gmail.com>
2022-10-06 01:51:13 +0200

    - Test to try to extend Vek's stuff...

---
+++ NewContent/NinjaTricks/FlurryOfStars.cs
@@ -0,0 +1,50 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Designers.Mechanics.Facts;
+using Kingmaker.Enums;
+using Kingmaker.ResourceLinks;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
+using Kingmaker.UnitLogic.Buffs.Components;
+using Kingmaker.UnitLogic.FactLogic;
+using Kingmaker.UnitLogic.Mechanics;
+using Kingmaker.Utility;
+using System.Diagnostics.Tracing;
+using Kingmaker.EntitySystem.Stats;
+using TabletopTweaks.Core.Utilities;
+using static LegacyOfShadows.Main;
+using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using Kingmaker.UnitLogic.Mechanics.Components;
+using LegacyOfShadows.NewComponents;
+using Kingmaker.Designers.EventConditionActionSystem.Actions;
+using Kingmaker.Visual.Animation.Kingmaker.Actions;
+using Kingmaker.UnitLogic.Commands.Base;
+using Kingmaker.UnitLogic.Abilities;
+using LegacyOfShadows.Utilities;
+using Kingmaker.UnitLogic.Abilities.Components;
+using HlEX = LegacyOfShadows.Utilities.HelpersExtension;
+using System.Drawing;
+using Kingmaker.UnitLogic.Buffs;
+using System;
+using Kingmaker.UnitLogic.Abilities.Components.Base;
+using Kingmaker.Blueprints.Items.Ecnchantments;
+using Kingmaker.Enums.Damage;
+using Kingmaker.RuleSystem;
+using Kingmaker.UnitLogic.Mechanics.Actions;
+using System.Linq;
+
+namespace LegacyOfShadows.NewContent.NinjaTricks
+{
+    // This has been called "Flurry of Stars" because I plan to re-introduce shurikens.
+
+
+    internal class FlurryOfStars
+    {
+        private static readonly string FlurryOfStarsFeatureName = "NinjaTrickFlurryOfStarseFeature.Name";
+        private static readonly string FlurryOfStarsFeatureDescription = "NinjaTrickFlurryOfStarsFeature.Description";
+        static public BlueprintFeature NinjaTrickFlurryOfStarsFeature;
+
+
+    }
+}
---
+++ NewUnitParts/AdditionalUnitPartCustomMechanicsFeatures.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.Enums.Damage;
+using Kingmaker.Items;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.RuleSystem.Rules.Damage;
+using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.FactLogic;
+using Kingmaker.Utility;
+using System;
+using System.Collections.Generic;
+using TabletopTweaks.Core.NewUnitParts;
+using static TabletopTweaks.Core.NewUnitParts.UnitPartCustomMechanicsFeatures;
+
+namespace LegacyOfShadows.NewUnitParts
+{
+    public class AdditionalUnitPartCustomMechanicsFeatures
+    {
+
+        //I have decided to start with a number above 12.000 (while Vek17 suggests > 1.000).
+
+
+        //private static int UseWeaponAsLightWeapon = 12_000;
+
+        //ClearMechanicsFeature((CustomMechanicsFeature) UseWeaponAsLightWeapon);
+
+        const TabletopTweaks.Core.NewUnitParts.UnitPartCustomMechanicsFeatures.CustomMechanicsFeature UseWeaponAsLightWeapon = (TabletopTweaks.Core.NewUnitParts.UnitPartCustomMechanicsFeatures.CustomMechanicsFeature)12000;
+
+
+
+    }
+}
